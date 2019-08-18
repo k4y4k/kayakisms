9efbdddbe5f150be3aa7a5c01429167ad841c5de
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-08-18 23:00:28 +1000

    Add first set of kayakisms

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-exports.printMsg = () => console.log("This is a message from the demo package");
+const strings = [
+  "That's pretty woomy.",
+  "I'm kayak and I'm here to tell you Marmite is garbo, Vegemite is king",
+  "*sigh* LISTEN,",
+  "I'm wakka kakka... Wakka! Kakka! kayak!",
+  "no wakkas mate",
+  "yeah nah, anyway...",
+  "don't capitalise kayak",
+  "I SAID *don't* capitalise kayak",
+  "bonza",
+  "yeah so anyway I came up with this idea while waiting in line at sevo levo (7-Eleven for the uninitiated)",
+  "could honestly smash 17,000 lamingtons right now, like seriously",
+  "I'm the goddamn Vegemite Fairy and that's a job I'm proud to do."
+];
+
+console.log(strings[Math.floor(Math.random() * strings.length)]);
+
+exports.beKayak = () => console.log("This is a message from the demo package");
